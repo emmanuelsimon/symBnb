8e73499b84742944b355965effc3187169734226
Emmanuel Simon <sie@gentel.fr>
2018-11-27 15:41:32 +0100

    first commit

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: user
+ * Date: 27/11/2018
+ * Time: 12:22
+ */
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+
+class HomeController extends Controller
+{
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function home() {
+        $prenom = ['lior'=>31, 'joseph'=>12, 'anne'=>55];
+        return $this->render(
+            'home.html.twig',
+            [
+                'title'=>'Bonjour à tous.......',
+                'age'=>17,
+                'tableau'=>$prenom
+            ]
+        );
+    }
+}
